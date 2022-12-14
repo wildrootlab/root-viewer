881d710056c59e20a256f69917164ab12758b673
Daniel Alas <81212794+DannyAlas@users.noreply.github.com>
2022-12-13 21:49:52 -0700

    fix documentation, add segmentation widgets

--- root_viewer/gui/main_window.py
+++ root_viewer/gui/main_window.py
@@ -1,10 +1,11 @@
 import os, sys
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QMainWindow,  QGridLayout, QWidget, QAction
-from PyQt5.QtCore import QSettings
+from PyQt5.QtWidgets import QMainWindow,  QGridLayout, QWidget, QAction, QDockWidget
+from PyQt5.QtCore import QSettings, Qt
 import ctypes
 from gui.napari_viewer import Viewer
 from gui.config import Settings
+DEBUG = os.getenv('DEBUG')
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # set the application user mode id for windows
@@ -15,21 +16,24 @@ class MainWindow(QMainWindow, Viewer, Settings):
     def __init__(self):
         super().__init__()
         self._main = QWidget()
-
+        self.debug=DEBUG
+        print(self.debug)
         self.settings = QSettings('Root-Viewer')
         self.get_settings()
         self.setWindowTitle("2P Analysis")
         self.setWindowIcon(QIcon(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "icons", "logo_light.png")))
         self.setCentralWidget(self._main)
 
-        # create a vertical box layout widget
+        # create a grid box layout widget
         self.layout = QGridLayout(self._main)
 
         # add an empty menuBar
         self.menu = self.menuBar()
 
         # add a napari viewer
-        self.layout.addWidget(self.qt_viewer, 1, 0)
+        self.layout.addWidget(self.qt_viewer, 0, 0)
+        self.create_analysis_widget()
+
         # populate the menuBar
         self.create_menue_bar()
         
@@ -39,6 +43,23 @@ class MainWindow(QMainWindow, Viewer, Settings):
         quit = QAction("Quit", self)
         quit.triggered.connect(self.closeEvent)
 
+    def create_analysis_widget(self):
+        """Creates the analysis widgets and adds them to the main window."""  
+        # This is a temporary method to add the analysis widget to the main window
+        # TODO: create private class for the analysis widget
+        
+        segmentation = self.segmentation_tab
+        segmentation._close_btn = False
+        segmentation._hide_btn = False
+        segmentation._float_btn = False
+
+        self.addDockWidget(Qt.RightDockWidgetArea, segmentation)
+        
+        stardist = self.stardist_tab
+        stardist._close_btn = False
+        stardist._hide_btn = False
+        stardist._float_btn = False
+        self.tabifyDockWidget(segmentation, stardist)
     
     def closeEvent(self, event):
         try:
